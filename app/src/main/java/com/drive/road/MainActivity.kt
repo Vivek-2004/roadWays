ccfd7b2b600425ac1f00ed5ca68d03c28e9901e9
Vivek Ghosh <ghoshvivek004@gmail.com>
2025-09-08 02:43:35 +0530

    Initial Commit

---
+++ app/src/main/java/com/drive/road/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.drive.road
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("Vivek")
+        }
+    }
+}
